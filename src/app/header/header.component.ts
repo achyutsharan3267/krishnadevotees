af3259b07f7bfa6de5a2fb934569e37221d93dc2
achyutsharan3267 <achyut.sharan@impressico.com>
2023-08-12 19:59:08 +0530

    custom js file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+declare function header(): any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  ngOnInit(): void {
+    header();
+  }
 }
---
+++ src/assets/js/custom.js
@@ -0,0 +1,10 @@
+function header(){
+    $(window).scroll(function () {
+        var sc = $(window).scrollTop()
+        if (sc > 100) {
+            $("#header-sroll").addClass("small")
+        } else {
+            $("#header-sroll").removeClass("small")
+        }
+    });
+}
